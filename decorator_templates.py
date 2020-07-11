a85792028905d4f63f6fc38c7508cee4ea6a3502
sharkvoice <sharknoise@gmail.com>
2020-07-11 13:55:25 +0300

    update examples

--- decorator_templates.py
+++ decorator_templates.py
@@ -1,3 +1,10 @@
+def decorator(function):
+    def inner(*args, **kwargs):
+        result = function(*args, **kwargs)
+        print('result =', result)
+        return result
+    return inner
+
 def decorator0(decorator0_setting):
     def wrapper(function):
         def inner(*args, **kwargs):  # decorated function_parameters
@@ -33,7 +40,7 @@ def decorator2(decorator2_setting):
         return inner
     return wrapper
 
-
+#@decorator
 @decorator1('setting1')
 @decorator2('setting2')
 def printer(text: str):
@@ -42,7 +49,11 @@ def printer(text: str):
 printer('function text')
 
 """
-декоратор как бы подменяет функцию - к ее коду добавляется содержимое inner
+декоратор как бы дополняет функцию - к ее коду добавляется содержимое inner
+чтобы выполнился сам код функции делается return
+
+можно не принимать *args и **kwargs, а потом вернуть только function без параметров, 
+если сам ее код не будет выполняться (как в hexlet interactive functions)
 
 Decorators are called at start time (when the python interpreter reads the code as the program starts), not at runtime (when the decorated function is actually called).
 At runtime, it is the wrapped function wrapper which is called and which itself calls the decorated function and returns its result.
